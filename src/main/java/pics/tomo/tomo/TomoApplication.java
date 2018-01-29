768c86568e48678f2d742c7addf02868462cca42
sschutzman <$t735815>
2018-01-29 11:26:59 -0600

    updated pom file

--- src/main/java/pics/tomo/tomo/TomoApplication.java
+++ src/main/java/pics/tomo/tomo/TomoApplication.java
@@ -2,11 +2,17 @@ package pics.tomo.tomo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TomoApplication {
+public class TomoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TomoApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TomoApplication.class);
 	}
 }
