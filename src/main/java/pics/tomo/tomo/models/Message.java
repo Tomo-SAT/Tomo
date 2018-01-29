17ce21b2da7ebc4260996bc74cbd0cc92fe96878
ashleycano <acano6835@gmail.com>
2018-01-29 17:08:32 -0600

    finished up message model

--- src/main/java/pics/tomo/tomo/models/Message.java
+++ src/main/java/pics/tomo/tomo/models/Message.java
@@ -21,6 +21,54 @@ public class Message {
     @Column
     private String body;
 
+    @ManyToOne
+    @JoinColumn (name = "con_schedule_id")
+    private ConSchedule conSchedule;
 
+    public Message(User creator, User recipient, String body, ConSchedule conSchedule) {
+        this.creator = creator;
+        this.recipient = recipient;
+        this.body = body;
+        this.conSchedule = conSchedule;
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public ConSchedule getConSchedule() {
+        return conSchedule;
+    }
+
+    public void setConSchedule(ConSchedule conSchedule) {
+        this.conSchedule = conSchedule;
+    }
 }
