35a9b8329dd3780df4de5728bce8949a8384001a
ashleycano <acano6835@gmail.com>
2018-02-06 15:41:24 -0600

    implemented search functionality and displayed value of checkboxes for cosplay and photographer on profile page

--- src/main/java/pics/tomo/tomo/controllers/ConsController.java
+++ src/main/java/pics/tomo/tomo/controllers/ConsController.java
@@ -28,9 +28,16 @@ public class ConsController {
     }
 
     @GetMapping("/calender")
-    public String calender(Model model)
+    public String showCalender(Model model)
     {model.addAttribute("cons",consRepo.findAll());
     return "cons/calender";
+    }
+
+    @GetMapping("/addCon")
+    public String showAddConPage(Model model) {
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        model.addAttribute("user", user);
+        return "cons/addCon";
     }
 
 }
